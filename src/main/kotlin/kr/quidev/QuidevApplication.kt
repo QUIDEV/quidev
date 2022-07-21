4c52af5966ab24401a04859106b885a37d7762fb
shane <psh40963@naver.com>
2022-07-21 23:56:06 +0900

    initial commit

---
+++ src/main/kotlin/kr/quidev/QuidevApplication.kt
@@ -0,0 +1,11 @@
+package kr.quidev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuidevApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuidevApplication>(*args)
+}
---
+++ src/test/kotlin/kr/quidev/QuidevApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.quidev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuidevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
