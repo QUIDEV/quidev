78e1f810a7a1bdaff9548bbdbc7bd56575f891b6
shane <psh40963@naver.com>
2022-08-09 23:04:09 +0900

    use WebSecurityCustomizer for ignore.
    also added DataInitializer for test account

---
+++ src/main/kotlin/kr/quidev/security/listener/DataInitializer.kt
@@ -0,0 +1,44 @@
+package kr.quidev.security.listener
+
+import kr.quidev.member.domain.entity.Member
+import kr.quidev.member.repository.MemberRepository
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class DataInitializer(
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder,
+) : ApplicationListener<ContextRefreshedEvent?> {
+    private var alreadySetup = false
+
+    @Transactional
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
+        if (alreadySetup) {
+            return
+        }
+        setupSecurityResources()
+        alreadySetup = true
+    }
+
+    private fun setupSecurityResources() {
+        createUserIfNotFound("shane", "1234", "shane")
+    }
+
+    @Transactional
+    fun createUserIfNotFound(
+        userName: String,
+        password: String,
+        email: String,
+    ): Member {
+        var member = memberRepository.findMemberByEmail(userName).orElse(null)
+        if (member == null) {
+            member = Member(userName, passwordEncoder.encode(password), email)
+        }
+        return memberRepository.save(member)
+    }
+
+}
